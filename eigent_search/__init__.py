c5a2b9c8e79d717dadd90f7954e29d7a2a09ae64
Yuan He <yuanhe.cs.ai@gmail.com>
2025-08-31 23:35:57 -0700

    feat: isolate eigent search agent

--- eigent_search/__init__.py
+++ eigent_search/__init__.py
@@ -11,8 +11,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========= Copyright 2025 @ CAMEL-AI.org. All Rights Reserved. =========
-
-from .research.eigent_search_agent import EigentSearchAgent
-
-__all__ = ["EigentSearchAgent"]
-
